6325745a221faf8a8c6086cdd544b37e71c58fef
monosolar <monosolar.in@gmail.com>
2016-01-14 00:04:01 +0100

    second commit

--- js/mainStage.js
+++ js/mainStage.js
@@ -1,242 +1,246 @@
-    var renderer;
-    var stage;
+var renderer;
+var stage;
 
-    var interval;
+var interval;
 
-    var dynamicBoxesArray = new Array();
-    var staticBoxesArray = new Array();
+var mapRect = {x:100, y:50,
+	with:mapCols * boxSize,
+	height:mapRows * boxSize};
 
+var dynamicBoxesArray = new Array();
+var staticBoxesArray = new Array();
 
-    var mapRect = {x:100, y:50,
-                   with:mapCols * boxSize,
-                   height:mapRows * boxSize};
-
-    initStage();
-
-    function initStage() {
-        renderer = PIXI.autoDetectRenderer(800, 600, { antialias: true });
-        document.body.appendChild(renderer.view);
-
-        stage = new PIXI.Container();
-        stage.interactive = true;
-
-        animate();
-
-        // scene
-        var bgGraphic = new PIXI.Graphics();
-        bgGraphic.lineStyle(1);
-        bgGraphic.beginFill(0xAFC8ED, 1);
-        bgGraphic.drawRect(0,0,mapRect.with,mapRect.height);
-        bgGraphic.endFill();
-        bgGraphic.x = mapRect.x;
-        bgGraphic.y = mapRect.y;
-        stage.addChild(bgGraphic);
-
-        newGame();
-
-    }
+var currentPivotBoxId;
 
 
-    function newGame() {
-        clearInterval(interval);
-        interval = setInterval( tick, 500 );
+initStage();
+
+function initStage()
+{
+	renderer = PIXI.autoDetectRenderer(800, 600, { antialias: true });
+	document.body.appendChild(renderer.view);
+	stage = new PIXI.Container();
+	stage.interactive = true;
+
+	animate();
+
+	var bgGraphic = new PIXI.Graphics();
+	bgGraphic.lineStyle(1);
+	bgGraphic.beginFill(0xAFC8ED, 1);
+	bgGraphic.drawRect(0,0,mapRect.with,mapRect.height);
+	bgGraphic.endFill();
+	bgGraphic.x = mapRect.x;
+	bgGraphic.y = mapRect.y;
+	stage.addChild(bgGraphic);
+
+	newGame();
+}
 
 
-        addFigure();
-    }
+function newGame() {
+	clearInterval(interval);
+	interval = setInterval( tick, 500 );
+
+	clearArray(staticBoxesArray);
+
+	addFigure();
+}
+
+function tick (direction) {
+
+	if (direction == undefined)
+		direction = 'down';
+
+	var figureRect = getFigureRect();
+
+	if (checkForOutOfMap(figureRect, direction)) return;
+
+	if (checkForLanding(figureRect))
+	{
+		moveFigureToStatic();
+
+		if (checkTopOfMap(figureRect))
+			newGame()
+		else
+			addFigure();
+
+		return;
+	}
+
+	shiftFigure(direction);
+}
+
+function addFigure()
+{
+	var figureInitColPos = 2;
+
+	var figureId = Math.floor(Math.random()*figuresArray.length);
+
+	var figureMap = figuresArray[figureId].figureMap;
+	var figureColor = figuresArray[figureId].figureColor;
+	currentPivotBoxId = figuresArray[figureId].figurePivotBoxId;
+
+	for (var i = 0; i < figureMap.length; i++)
+	{
+		if (figureMap[i] != 0)
+		{
+			var box = addBox(figureColor);
+			box.x = (figureInitColPos + i % 4) * boxSize + mapRect.x;
+			box.y = Math.floor(i / 4) * boxSize  + mapRect.y;
+			dynamicBoxesArray.push(box);
+			stage.addChild(box);
+		}
+	}
+}
+
+function addBox(color)
+{
+	var graphics = new PIXI.Graphics();
+
+	graphics.lineStyle(1);
+	graphics.beginFill(color, 1);
+	graphics.drawRect(0,0,boxSize,boxSize);
+	graphics.endFill();
+	
+	return graphics;
+}
 
 
 
-    function addFigure() {
+function getFigureRect() {
+	var figureRect = {  left:dynamicBoxesArray[0].x,            top:dynamicBoxesArray[0].y,
+						right:dynamicBoxesArray[0].x + boxSize, bottom:dynamicBoxesArray[0].y + boxSize };
 
-        var figureInitColPos = 2;
+	var currentBox;
+	for (var i = 1; i < dynamicBoxesArray.length; i++)
+	{
+		currentBox = dynamicBoxesArray[i];
 
-        /*var box = addBox(figuresArray[3].figureColor);
-        box.x = 3 * boxSize + mapRect.x;
-        box.y = 1 * boxSize  + mapRect.y;
-        dynamicBoxesArray.push(box);
-        stage.addChild(box);*/
+		figureRect.left = Math.min(figureRect.left, currentBox.x);
+		figureRect.right = Math.max(figureRect.right, currentBox.x + boxSize);
+		figureRect.bottom = Math.max(figureRect.bottom, currentBox.y + boxSize);
+	}
 
-        var figureId = Math.floor(Math.random()*figuresArray.length);
-
-        var figureMap = figuresArray[figureId].figureMap;
-        var figureColor = figuresArray[figureId].figureColor;
-
-        for (var i = 0; i < figureMap.length; i++)
-        {
-            if (figureMap[i] != 0)
-            {
-                var box = addBox(figureColor);
-                box.x = (figureInitColPos + i % 4) * boxSize + mapRect.x;
-                box.y = Math.floor(i / 4) * boxSize  + mapRect.y;
-                dynamicBoxesArray.push(box);
-                stage.addChild(box);
+	return figureRect;
+}
 
 
-            }
+function checkForLanding(figureRect) {
 
-        }
-    }
+	if  (figureRect.bottom >= mapRect.height + mapRect.y) return true;
 
-    function addBox(color) {
+	for (var i = 0; i < staticBoxesArray.length; i++)
+	{
+		var staticBox = staticBoxesArray[i];
 
-        var graphics = new PIXI.Graphics();
+		if ( (staticBox.x >= figureRect.left) &&
+			 (staticBox.x <= figureRect.right) &&
+			 (staticBox.y <= figureRect.bottom) )
+		{
+			return true
+		}
+	}
 
-        graphics.lineStyle(1);
-        graphics.beginFill(color, 1);
-        graphics.drawRect(0,0,boxSize,boxSize);
-        graphics.endFill();
+	return false;
+}
 
-        graphics.lineStyle(1);
-        graphics.beginFill(0x000000, 1);
-        graphics.drawEllipse(0,0,2,2);
-        graphics.endFill();
+function checkForOutOfMap(figureRect, direction)
+{
+	if (    (direction == RIGHT_MOVE && figureRect.right >= mapRect.with + mapRect.x) ||
+			(direction == LEFT_MOVE && figureRect.left <= mapRect.x))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
-        return graphics;
-    }
+}
 
-    function tick (direction) {
+function checkTopOfMap(figureRect)
+{
+	if (figureRect.top <= mapRect.x)
+		return true
+	else
+		return false;
+}
 
-        if (direction == undefined)
-            direction = 'down';
+function shiftFigure(direction)
+{
+	var box;
 
-        if (checkForOutOfMap(getDirectionBorder(direction),direction)) return;
+	if (direction == ROTATE_MOVE)
+	{
+		rotateFigure();
+		return;
+	}
 
-        shiftFigure(direction);
+	for (var i = 0; i < dynamicBoxesArray.length; i++)
+	{
+		box = dynamicBoxesArray[i];
+		switch(direction) {
+			case LEFT_MOVE:
+				box.x -= boxSize;
+				break;
+			case RIGHT_MOVE:
+				box.x += boxSize;
+				break;
+			case DOWN_MOVE:
+				box.y += boxSize;
+				break;
+		}
+	}
+}
 
-       /* if (checkForLanding(box))
-        {
-            moveFigureToStatic();
-            addFigure();
-            return;
-        }*/
+function rotateFigure()
+{
+	var currentBox;
+	var centerBox = dynamicBoxesArray[currentPivotBoxId];
+	var x1,x2,y1,y2;
+	for (var i = 0; i < dynamicBoxesArray.length; i++)
+	{
+		currentBox = dynamicBoxesArray[i];
 
+		x1 = currentBox.x;
+		y1 = currentBox.y;
 
-    }
+		x1 -= centerBox.x;
+		y1 -= centerBox.y;
 
-    /*function getFigureBorder(direction) {
+		x2 = - y1;
+		y2 = x1;
 
-        switch(direction) {
-            case DOWN_MOVE:
-                dynamicBoxesArray.sort(compareBotomest);
-                return dynamicBoxesArray[0].y;
+		x2 += centerBox.x;
+		y2 += centerBox.y;
 
-            case LEFT_MOVE:
-                dynamicBoxesArray.sort(compareLeftest);
-                console.log("direct",dynamicBoxesArray[0].x);
-                return dynamicBoxesArray[0].x;
+		currentBox.x = x2;
+		currentBox.y = y2;
+	}
 
-            case RIGHT_MOVE:
-                dynamicBoxesArray.sort(compareRightest);
-                console.log("direct",dynamicBoxesArray[0].x);
-                return dynamicBoxesArray[0].x;
+}
 
-        }
+function clearArray(array)
+{
+	do
+	{
+		stage.removeChild(array.pop());
+	}
+	while (array.length > 0);
+}
 
-    }*/
-
-    /*function compareLeftest(a, b) {
-        return Math.max(a.x, b.x) ? 1 : -1;
-    }
-
-    function compareRightest(a, b) {
-        return Math.min(a.x, b.x) ? 1 : -1;
-    }
-
-    function compareBotomest(a, b) {
-        return Math.max(a.y, b.y) ? 1 : -1;
-    }*/
-
-    function getDirectionBorder(direction) {
-        var resultBorder;
-        var currentBox,prevBox;
-        for (var i = 1; i < dynamicBoxesArray.length; i++)
-        {
-            currentBox = dynamicBoxesArray[i];
-            prevBox = dynamicBoxesArray[i-1];
-            switch(direction) {
-                case LEFT_MOVE:
-                    resultBorder = Math.min(currentBox.x, prevBox.x);
-                    break;
-                case RIGHT_MOVE:
-                    resultBorder = Math.max(currentBox.x, prevBox.x);
-                    break;
-                case DOWN_MOVE:
-                    resultBorder = Math.max(currentBox.y, prevBox.y);
-                    break;
-            }
-        }
-
-        switch(direction) {
-            case LEFT_MOVE:
-            case RIGHT_MOVE:
-                console.log("die", resultBorder);
-                break;
-        }
-
-        return resultBorder;
-    }
-
-
-    function checkForLanding(box) {
-
-        if  ((box.y+boxSize) >= mapRect.height + mapRect.y) return true;
-
-        for (var i = 0; i < staticBoxesArray.l1ength; i++)
-        {
-            var staticBox = staticBoxesArray[i];
-
-            if ( (box.x == staticBox.x) && ( box.y + boxSize >= staticBox.y ) )
-            {
-                return true
-            }
-        }
-
-        return false;
-    }
-
-    function checkForOutOfMap(borderX, direction) {
-        if (    (direction == RIGHT_MOVE && borderX+boxSize >= mapRect.with + mapRect.x) ||
-                (direction == LEFT_MOVE && borderX <= mapRect.x))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
-
-    function shiftFigure(direction) {
-        var box;
-        for (var i = 0; i < dynamicBoxesArray.length; i++) {
-            box = dynamicBoxesArray[i];
-            switch(direction) {
-                case LEFT_MOVE:
-                    box.x -= boxSize;
-                    break;
-                case RIGHT_MOVE:
-                    box.x += boxSize;
-                    break;
-                case DOWN_MOVE:
-                    box.y += boxSize;
-                    break;
-            }
-        }
-
-    }
-
-    function moveFigureToStatic() {
-        do
-        {
-            staticBoxesArray.push( dynamicBoxesArray.pop() );
-        }
-        while (dynamicBoxesArray.length > 0);
-    }
+function moveFigureToStatic()
+{
+	do
+	{
+		staticBoxesArray.push( dynamicBoxesArray.pop() );
+	}
+	while (dynamicBoxesArray.length > 0);
+}
 
 
-    function animate() {
-        renderer.render(stage);
-        requestAnimationFrame( animate );
-    }
+function animate()
+{
+	renderer.render(stage);
+	requestAnimationFrame( animate );
+}
--- js/mapConsts.js
+++ js/mapConsts.js
@@ -7,36 +7,42 @@ const figuresArray = [
 
     {figureMap:
         [   1,1,1,1     ],
-        figureColor: 0xFF1493 },
+    figureColor: 0xFF1493,
+    figurePivotBoxId: 1},
 
     {figureMap:
         [   1,1,1,0,
             1,0,0,0     ],
-        figureColor: 0x00FFFF },
+    figureColor: 0x00FFFF,
+    figurePivotBoxId: 1},
 
     {figureMap:
         [   1,1,1,0,
             0,0,1,0     ],
-        figureColor: 0x228B22 },
+    figureColor: 0x228B22 ,
+    figurePivotBoxId: 1},
 
     {figureMap:
         [   1,1,0,0,
             1,1,0,0     ],
-        figureColor: 0xFFD700 },
-
+    figureColor: 0xFFD700 ,
+    figurePivotBoxId: 1},
     {figureMap:
         [   1,1,0,0,
             0,1,1,0     ],
-        figureColor: 0xFF00FF },
+    figureColor: 0xFF00FF ,
+    figurePivotBoxId: 1},
 
     {figureMap:
         [   0,1,1,0,
             1,1,0,0     ],
-        figureColor: 0x6A5ACD },
+    figureColor: 0x6A5ACD ,
+    figurePivotBoxId: 3},
 
     {figureMap:
-        [   0,1,0,0,
-            1,1,1,0     ],
-        figureColor: 0xDC143C }
+        [   1,1,1,0,
+            0,1,0,0     ],
+    figureColor: 0xDC143C ,
+    figurePivotBoxId: 1}
 
 ];
